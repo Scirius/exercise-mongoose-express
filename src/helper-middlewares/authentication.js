721320af020d46c349d14b1de06c332ad393e913
Scirius <68830011+Scirius@users.noreply.github.com>
2020-10-05 11:32:40 +0200

    added auth middleware module

---
+++ src/helper-middlewares/authentication.js
@@ -0,0 +1,6 @@
+const jwt = require("jwt");
+const authentication = {
+generateAuth: async(req, res, next) => {}, // we use async because we are going to
+isAuthenticated: async(req, res, next) => {}
+}
+module.exports = authentication;
